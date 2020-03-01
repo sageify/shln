1d99178f0fb41808794bf20470157bac1f7966bd
Sean Drucker <srdrucker@gmail.com>
2020-03-01 08:49:18 -0600

    add uninstall

--- shln_uninstall.sh
+++ shln_uninstall.sh
@@ -2,20 +2,24 @@
 
 # for example: shln install github.com/dockcmd/aws v0.0.2
 
+if [ "$1" = "github.com/sageify/shln" ]; then
+  echo Cannot uninstall $1
+  exit 1
+fi
+
 SHLN_PATH=${SHLN_PATH-~/shln}
 
 repo=$SHLN_PATH/$1
 
-if [ -f "$repo" ] || [ -d "$repo" ]; then
-  echo Repository already exists: $repo
+if ! [ -d "$repo/.git" ]; then
+  echo Git repository does not exists: $repo
   exit 1
 fi
 
-mkdir -p $repo
-git clone --branch $2 --depth 1 https://$1.git $repo
-
 if [ -f $repo/shln.conf ]; then 
   while IFS= read link; do
-    shln ln $link
+    shln rm $link
   done < $repo/shln.conf
 fi
+
+rm -rf $repo
