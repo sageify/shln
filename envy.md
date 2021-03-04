77a90a0330b0fc0d5a3a03707d9c7f318bf92925
Sean Drucker <srdrucker@gmail.com>
2021-03-04 01:04:51 -0500

    add domain

--- envy.md
+++ envy.md
@@ -4,28 +4,65 @@ Envy manages enviornment variable contexts.  A context consists of a context nam
 
 ```sh
 # unload any previous context
-nv unload
-
-# declare pattern
-nv pattern "^GRM_|^HELLO"
-export GRM_DEFAULT_ORG=sageify
-export HELLO='Hello World!'
+nv new '^SAY'
+get SAY='Hello World!'
+nv put hello
 
 # show variables under management
 nv
 
-# save default context
-nv save
+# show saved file
+nv cat
 
 # context set to default
 nv context
 
-# unload context
-nv unload
+# unset pattern '^SAY'
+nv unset
 
-# no context
-nv context
+# nothing to see
+nv
+
+# get current context again
+nv get
 ```
+
+## Directories
+
+Directories provide a way to store mutual exclusive environment contexts.  In other words, only one context may be loaded from a directory at the same time.
+
+For example, only one git configuration should be available at one time.
+
+``sh
+nv new '^SAY='
+get SAY='Hello World!'
+nv put hello
+
+nv new '^GIT_'
+get GIT_COMMITTER_NAME='John Doe'
+get GIT_COMMITTER_EMAIL='john@example.com'
+get GIT_AUTHOR_NAME='John Doe'
+get GIT_AUTHOR_EMAIL='john@example.com'
+nv put git/john
+
+nv new '^GIT_'
+get GIT_COMMITTER_NAME='Jane Doe'
+get GIT_COMMITTER_EMAIL='jane@example.com'
+get GIT_AUTHOR_NAME='Jane Doe'
+get GIT_AUTHOR_EMAIL='jane@example.com'
+nv put git/jane
+
+nv new '^SAY='
+get SAY='Goodbye!'
+nv put goodbye
+
+nv get hello
+nv
+
+
+``
+
+
 
 ## Install
 
@@ -53,7 +90,7 @@ nv load
 # load default
 nve load
 
-# nvy export
+# nvy get
 nve load c
 
 typeset -f
--- nv.sh
+++ nv.sh
@@ -1,70 +1,238 @@
 nv() {
   case "$1" in
-  "") [ "$ENVY_PATTERN" ] && env | grep -E "$ENVY_PATTERN" ;;
+  "")
+    nv grep "$ENVY_PATTERN"
+    ;;
+  all)
+    case "$2" in
+    "")
+      nv domain all | while read all_domain; do
+        nv grep $(nv domain pattern "$all_domain")
+      done
+      ;;
+    unset)
+      nv domain all | while read all_unset_domain; do
+        nv domain unset "$all_unset_domain"
+        unset "ENVY_DOMAIN_$all_unset_domain"
+      done
+      nv unset "$ENVY_PATTERN"
+      unset ENVY_PATTERN
+      unset ENVY_CONTEXT
+      ;;
+    help)
+      echo "usage: nv all [ unset | help ]"
+      ;;
+    *)
+      echo "all: $2: Invalid command" 1>&2
+      nv all help 1>&2
+      return 1
+      ;;
+    esac
+    ;;
   context)
-    if [ "$2" ]; then
-      export ENVY_CONTEXT="$2"
-    else
+    case "$2" in
+    "")
       [ "$ENVY_CONTEXT" ] && echo $ENVY_CONTEXT
-    fi
+      ;;
+    set)
+      nv context check $3 && [ "$3" ] && export ENVY_CONTEXT="$3"
+      ;;
+    check)
+      check_context=${3-$ENVY_CONTEXT}
+      case "$check_context" in
+      "")
+        echo "context: May not be empty" 1>&2
+        return 1
+        ;;
+      /* | */)
+        echo "context: $check_context: Cannot have leading or trailing slash" 1>&2
+        return 1
+        ;;
+      *[![:alnum:]/]*)
+        echo "context: $check_context: Invalid path character, can only be alphanumeric" 1>&2
+        return 1
+        ;;
+      esac
+      ;;
+    help)
+      echo "usage: nv context [ set | check [CONTEXT] ]"
+      ;;
+    *)
+      echo "context: $2: Invalid command" 1>&2
+      nv context help 1>&2
+      return 1
+      ;;
+    esac
     ;;
-  env) env | grep -E "^ENVY_" ;;
-  grep) env | grep -E "$2" ;;
-  home) echo "$ENVY_HOME" ;;
-  pattern)
-    if [ "$2" ]; then
-      export ENVY_PATTERN="$2"
+  domain)
+    if [ -n "${3+x}" ]; then
+      case "$2" in
+      check)
+        case "$3" in
+        /* | */ | */*)
+          echo "domain: $2: Cannot have leading or trailing slash" 1>$2
+          return 1
+          ;;
+        *[![:alnum:]]*)
+          echo "domain: $2: Invalid character, can only be alphanumeric" 1>$2
+          return 1
+          ;;
+        esac
+        ;;
+      context)
+        context_dv=$(nv domain value "$3") && echo ${context_dv%:*}
+        ;;
+      pattern)
+        pattern_dv=$(nv domain value "$3") && echo ${pattern_dv#*:}
+        ;;
+      unset)
+        nv unset "$(nv domain pattern "$3")"
+        ;;
+      value)
+        nv domain check "$3" && echo $(printenv ENVY_DOMAIN_$3)
+        ;;
+      esac
     else
-      [ "$ENVY_PATTERN" ] && echo $ENVY_PATTERN
+      case "$2" in
+      "")
+        ! nv context check && return 1
+        case "$ENVY_CONTEXT" in
+        */*)
+          # domain is the first part of a path in a directory
+          echo "${ENVY_CONTEXT%/${ENVY_CONTEXT#*/}}"
+          ;;
+        esac
+        ;;
+      all)
+        IFS='='
+        nv grep '^ENVY_DOMAIN_' | while read key _; do
+          echo ${key#ENVY_DOMAIN_}
+        done
+        ;;
+      set)
+        set_domain=$(nv domain) && export ENVY_DOMAIN_$set_domain="$ENVY_CONTEXT:$ENVY_PATTERN"
+        ;;
+      help)
+        cat <<EOF
+usage: domain COMMAND
+
+General Commands
+all   show all active domains
+set   set current context and pattern as domain
+help  show this help
+
+Domain Commands
+check     check if valid name
+context   show the context
+pattern   show the pattern
+unset     unset all environment variables
+value     show value of the domain environment variable
+EOF
+        ;;
+      check | context | pattern | unset | value)
+        echo "usage: nv domain $2 DOMAIN" 1>&2
+        return 1
+        ;;
+      *)
+        echo "domain: $2: Invalid command" 1>&2
+        nv domain help 1>&2
+        return 1
+        ;;
+      esac
     fi
     ;;
-  unload)
-    nv unset
+  env)
+    nv grep "^ENVY_"
+    ;;
+  home)
+    echo "$ENVY_HOME"
+    ;;
+  pattern)
+    case "$2" in
+    "")
+      [ "$ENVY_PATTERN" ] && echo $ENVY_PATTERN
+      ;;
+    set)
+      [ "$3" ] && export ENVY_PATTERN="$3" || unset ENVY_PATTERN
+      ;;
+    unset)
+      nv unset $ENVY_PATTERN
+      ;;
+    help)
+      echo "usage: pattern [ set [PATTERN] | unset ]"
+      ;;
+    *)
+      echo "pattern: $2: Invalid command" 1>&2
+      nv pattern help 1>&2
+      return 1
+      ;;
+    esac
+    ;;
+  grep)
+    [ "$2" ] && env | grep -E "$2"
+    ;;
+  new)
+    if ! [ "$2" ]; then
+      echo "usage: new PATTERN"
+      return 1
+    fi
+    nv unset "$2"
+    export ENVY_PATTERN="$2"
     unset ENVY_CONTEXT
-    unset ENVY_PATTERN
+    ;;
+  switch)
+    ! get_dv=$(nv domain value "$2") && return 1
+    if ! [ "$get_dv" ]; then
+      echo "switch: "${2:-<root>}": Domain not found" 1>&2
+      return 1
+    fi
+    ENVY_CONTEXT=${get_dv%:*}
+    ENVY_PATTERN=${get_dv#*:}
     ;;
   unset)
-    ! [ "${2-$ENVY_PATTERN}" ] &&
-      return 0
-    IFS=
-    env | grep -E "${2-$ENVY_PATTERN}" | while read kv; do
-      unset $(echo $kv | cut -f1 -d"=")
+    IFS='='
+    nv grep "$2" | while read key _; do
+      unset $key
     done
     ;;
   uninstall)
+    nv all unset
     unset ENVY_HOME
-    unset ENVY_CONTEXT
-    unset ENVY_PATTERN
     unset -f nv
     ;;
-  version) echo "nv version 0.1.0" ;;
+  version) echo "envy version 0.1.1" ;;
   help)
     cat <<EOF
 usage:  nv COMMAND
 
-Environment Commands
- context    show the current context
- env        show ENVY_ environment variables
- grep       grep environment to extended pattern
- home       show saved contexts home directory
- pattern    show the pattern for the context
- unload     unset environment variables, context and pattern
- unset      unset environment variables
- version    show nv version
+Environment 
+ get      add context to current environemnt
+ grep     grep all environment to an extended pattern
+ put      create or replace stored context
+ switch   switch to context of another domain
+ unset    unset all environment variables for pattern
 
-Context File Management
- cat   show saved context
- cd    change to context file directory
- cp    copy saved context
- load  load context
- ls    list saved contexts    
- mv    move saved context
- rm    remove saved context
- save  save context
+Resources
+ all       show environments for all domains
+ context   show the current context
+ domain    show the current domain, all domains, or context
+ env       show ENVY_ environment variables
+ home      show saved contexts home directory
+ pattern   show the pattern for the context
+ 
+Directory Management
+ cat     show saved context
+ cd      change to context file directory
+ cp      copy saved context
+ ls      list saved contexts
+ mkdir   make directory  
+ mv      move saved context
+ rm      remove saved context
 
 General Commands
- help       show this help
- uninstall  unset environment variables, context, pattern and nv
+ help        show this help
+ uninstall   unset everything and uninstall nv
+ version     show nv version
 EOF
     ;;
   *)
@@ -73,35 +241,58 @@ EOF
       return 99
     fi
 
+    #
+    # These commands require ENVY_HOME to be set.
+    #
     case "$1" in
-    load)
-      context=${2-${ENVY_CONTEXT-default}}
-      file="$ENVY_HOME/$context"
-      if ! [ -f "$file" ]; then
-        echo "load: $file: File not found" 1>&2
+    get)
+      get_context=${2-${ENVY_CONTEXT}}
+      if ! [ "$get_context" ]; then
+        echo 'get: context not set:  Use "nv get CONTEXT"' 1>&2
         return 1
       fi
 
-      nv unload
-      nv context "$context"
-      . $file
+      get_file="$ENVY_HOME/$get_context"
+      if ! [ -f "$get_file" ]; then
+        echo "get: $get_file: File not found" 1>&2
+        return 1
+      fi
+
+      ! nv context set "$get_context" && return 1
+
+      nv domain unset "$(nv domain)"
+      . $get_file
+      nv domain set
       ;;
-    save)
+    put)
       if ! [ "$ENVY_PATTERN" ]; then
-        echo 'save: grep pattern not set:  Use "nv pattern" first to set grep pattern' 1>&2
+        echo 'put: pattern not set:  Use "nv pattern set" first to set the pattern' 1>&2
         return 1
       fi
 
-      nv context "${2-${ENVY_CONTEXT-default}}"
-      file="$ENVY_HOME/$ENVY_CONTEXT"
-      echo 'export ENVY_PATTERN='\'$(echo $ENVY_PATTERN | sed "s/'/'\\\\''/g")\' >$file
+      put_context="${2-${ENVY_CONTEXT}}"
+      if ! [ "$put_context" ]; then
+        echo 'put: context not set:  Use "nv put CONTEXT"' 1>&2
+        return 1
+      fi
+
+      ! nv context set "$put_context" && return 1
+
+      nv domain set
+
+      put_file="$ENVY_HOME/$ENVY_CONTEXT"
+      echo 'export ENVY_PATTERN='\'$(echo $ENVY_PATTERN | sed "s/'/'\\\\''/g")\' >$put_file
       IFS='='
-      env | grep -E "$ENVY_PATTERN" | while read name val; do
-        echo "export $name="\'$(echo $val | sed "s/'/'\\\\''/g")\' >>$file
+      nv grep "$ENVY_PATTERN" | while read name val; do
+        echo "export $name="\'$(echo $val | sed "s/'/'\\\\''/g")\' >>$put_file
       done
       ;;
-    cat) [ "${2-$ENVY_CONTEXT}" ] && cat "$ENVY_HOME/${2-$ENVY_CONTEXT}" ;;
-    cd) cd "$ENVY_HOME" ;;
+    cat)
+      [ "${2-$ENVY_CONTEXT}" ] && cat "$ENVY_HOME/${2-$ENVY_CONTEXT}"
+      ;;
+    cd)
+      cd "$ENVY_HOME"
+      ;;
     cp)
       if ! [ "$2" ] || ! [ "$3" ]; then
         echo "usage: cp SOURCE DEST"
@@ -109,7 +300,16 @@ EOF
       fi
       cp "$ENVY_HOME/$2" "$ENVY_HOME/$3"
       ;;
-    ls) ls "$ENVY_HOME/$2" ;;
+    ls)
+      if [ "${2%-*}" ]; then
+        ls "$ENVY_HOME/$2"
+      else
+        ls $2 "$ENVY_HOME/$3"
+      fi
+      ;;
+    mkdir)
+      mkdir -p "$ENVY_HOME/$2"
+      ;;
     mv)
       if ! [ "$2" ] || ! [ "$3" ]; then
         echo "usage: mv SOURCE DEST"
@@ -124,8 +324,16 @@ EOF
       fi
       rm "$ENVY_HOME/$2"
       ;;
+    which)
+      which="$ENVY_HOME/$2"
+      if ! [ -f "$which" ] && ! [ -d "$which" ]; then
+        echo "$which not found" 1>&2
+        return 1
+      fi
+      echo "$which"
+      ;;
     *)
-      echo "env: $1: Invalid command" 1>&2
+      echo "nv: $1: Invalid command" 1>&2
       nv help 1>&2
       return 1
       ;;
@@ -135,4 +343,4 @@ EOF
 }
 
 ! [ "$ENVY_HOME" ] && export ENVY_HOME="${2-$HOME/.config/envy}"
-[ -f "$ENVY_HOME/$1" ] && nv load "$1"
+[ -f "$ENVY_HOME/$1" ] && nv get "$1"
