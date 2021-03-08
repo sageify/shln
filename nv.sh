383e572be0de683fbaa705a59e3dbd1e93b058d1
Sean Drucker <srdrucker@gmail.com>
2021-03-08 08:09:57 -0500

    public expansion

--- nv.sh
+++ nv.sh
@@ -2,74 +2,115 @@ nv() {
   ENVY_HOME="${ENVY_HOME-$HOME/.config/envy}"
 
   case "$1" in
-  '') nv -g "$(nv -p)" ;;
+  '') nv -g "$(nv -pp)" ;;
+
   all | -a)
-    for __ in $(nv -pa); do
+    shift && case "$1" in
+    -d) nv -da "$@" ;;
+    -e) nv -de "$@" ;;
+    -h) echo "usage: all -dehnp" && return 0 ;;
+    -n) nv -na "$@" ;;
+    -p) nv -pa "$@" ;;
+    -*) echo "all: $1: Unknown option" 1>&2 && nv all -h 1>&2 && return 1 ;;
+    *) nv -aa "$@" ;;
+    esac
+    ;;
+  -aa)
+    nv _nea "$2" "all" && for __ in $(nv -pa); do
       nv -g "$__"
     done
     ;;
 
-  cat) cat "$ENVY_HOME/$(nv -r "${2-$(nv -n)}")" ;;
+  cat) cat "$ENVY_HOME/$(nv -r "${2-$(nv -nn)}")" ;;
 
-  cd) cd "$ENVY_HOME/$(nv -d)" ;;
+  cd) cd "$ENVY_HOME/$(nv -dd)" ;;
 
-  close | -c | -ec | -ce)
-    case "$2" in
-    '') nv -c "$(nv -d)" ;;
-    *)
-      ! nv -du "$2" && return 1
-
-      if [ "$(nv -d)" = "$2" ]; then
-        nv -pu
-        nv -d nv
-      fi
-
-      unset envy_domain_$2
-      ;;
+  close | -c)
+    shift && case "$1" in
+    -a) shift && -ca "$@" ;;
+    '') nv close $(nv -dd) ;;
+    *) nv -ec "$@" && nv -ee nv ;;
     esac
     ;;
   -ca | -ac)
     nv -pc
     for __ in $(nv -da); do
-      nv -dc $__
+      nv -ec $__
     done
-    unset -v envy_context
+    unset -v envy_name
+    ;;
+
+  # internal - no extra arguments
+  _nea)
+    ! [ "$2" ] && return 0
+    echo "$3 $2: Too many arguments" 1>&2
+    return 1
     ;;
 
   domain | -d)
-    if [ "$2" ]; then
-      ! nv -ev "$2" 1>/dev/null && return 1
-      nv -n "$(nv -en "$2")"
-      nv -p "$(nv -ep "$2")"
-    else
-      nv -rd "$envy_context"
-    fi
+    shift && case "$1" in
+    -a) shift && nv -da "$@" ;;
+    -c) shift && nv -dc "$@" ;;
+    -f) shift && nv -df "$@" ;;
+    -h) echo "usage: domain -acdhu" && return 0 ;;
+    -u) shift && nv -du "$@" ;;
+    -*) echo "domain: $1: Unknown option" 1>&2 && nv domain -h 1>&2 && return 1 ;;
+    *) nv -dd "$@" ;;
+    esac
     ;;
+  -dd) nv _nea "$2" "domain" && nv -rd "$envy_name" ;;
   -da | -ad)
-    set | grep '^envy_domain_' | while IFS='=' read key _; do
-      echo ${key#envy_domain_}
+    nv _nea "$2" "domain -a" && set | grep '^envy_env_' | while IFS='=' read key _; do
+      echo ${key#envy_env_}
     done
     ;;
-  -du | -ud) __="$(nv -ep "${2-$(nv -d)}")" && nv -pu "$__" ;;
+  -dc | -cd) nv _nea "$2" "domain -c" && nv -ec $(nv -dd) ;;
   -df | -fd)
-    find "$ENVY_HOME" -mindepth 1 -maxdepth 1 -type d -path "*$2" | while read file; do
-      printf %s\\n "${file#$ENVY_HOME/}"
+    nv _nea "$3" "domain -f $2" &&
+      find "$ENVY_HOME" -mindepth 1 -maxdepth 1 -type d -path "*$2" | while read file; do
+        printf %s\\n "${file#$ENVY_HOME/}"
+      done
+    ;;
+  -du | -ud) nv _nea "$2" "domain -u" && nv -eu $(nv -dd) ;;
+
+  env | -e)
+    shift && case "$1" in
+    -a) shift && nv -ea "$@" ;;
+    *) nv -ee "$@" ;;
+    esac
+    ;;
+  -ee)
+    ! nv -ev "$2" 1>/dev/null && return 1
+    nv -nn "$(nv -en "$2")"
+    nv -pp "$(nv -ep "$2")"
+    ;;
+  -ea | -ae)
+    nv _nea "$2" "env -a" && for __ in $(nv -da); do
+      echo $(nv -ev $__)
     done
     ;;
-  -dc | -cd) nv -du "$2" && unset envy_domain_$2 ;;
-
-  env | -e | -ev | -ve) [ "$2" ] && __="$(eval 'echo $envy_domain_'$(nv -rd "$2"))" &&
-    [ "$__" ] && echo "$__" ;;
+  -ec | -ce) nv -eu "$2" && unset envy_env_$2 ;;
   -en | -ne) __="$(nv -ev "$2")" && echo "${__%=${__#*=}}" ;;
   -ep | -pe) __="$(nv -ev "$2")" && echo "${__#*=}" ;;
+  -eu | -ue) __="$(nv -ep "$2")" && nv -pu "$__" ;;
+  -ev | -ve) [ "$2" ] && __="$(eval 'echo $envy_env_'$(nv -rd "$2"))" &&
+    [ "$__" ] && echo "$__" ;;
 
   grep | -g) [ "$2" ] && env | grep -Ee "$2" ;;
 
   name | -n)
+    shift && case "$1" in
+    '') nv -nn "$@" ;;
+    -a) shift && nv -na "$@" ;;
+    -f) shift && nv -nf "$@" ;;
+    -h) echo "usage: name -afh" && exit 0 ;;
+    esac
+    ;;
+  -nn)
     if [ -n "${2+x}" ]; then
-      __=$(nv -r "$2") && envy_context=$__ && echo $__
+      __=$(nv -r "$2") && envy_name=$__ && echo $__
     else
-      nv -r "$envy_context"
+      nv -r "$envy_name"
     fi
     ;;
   -na | -an)
@@ -83,30 +124,31 @@ nv() {
     done
     ;;
   -ff)
-    find "$ENVY_HOME/$(nv -d)" -mindepth 1 -maxdepth 1 -type f -name "$2" | while read file; do
-      printf %s\\n "${file#$ENVY_HOME/$(nv -d)/}"
+    find "$ENVY_HOME/$(nv -dd)" -mindepth 1 -maxdepth 1 -type f -name "$2" | while read file; do
+      printf %s\\n "${file#$ENVY_HOME/$(nv -dd)/}"
     done
     ;;
 
   new)
     nv -pu
-    nv -p "${2-$(nv -ep $(nv -d))}"
+    nv -pp "${2-$(nv -ep $(nv -dd))}"
     nv -pu
     ;;
 
   open | -o)
     shift
-    ! [ "$1" ] && set -- $(nv -n)
+    ! [ "$1" ] && set -- $(nv -nn)
 
     for __ in "$@"; do
-      ! nv -n "$__" && return 1
-      nv -p ""
-      nv -du $(nv -d)
+      ! nv -nn "$__" && return 1
+      nv -pp ""
+      nv -du
 
       __=x
       while read _nv_line; do
         if [ $__ ]; then
-          nv -p "$_nv_line"
+          nv -pp "$_nv_line"
+          eval 'envy_env_'$(nv -dd)'="$(nv -nn)=$(nv -pp)"'
           unset __
           continue
         fi
@@ -115,19 +157,25 @@ nv() {
 $_nv_line
 EOF
         case "$_nv_key" in
-        ENVY_HOME | ^envy_domain* | envy_context | '') ;;
+        ENVY_HOME | ^envy_domain* | envy_name | '') ;;
         *)
           export $_nv_key="$_nv_value"
           ;;
         esac
-      done <"$ENVY_HOME/$(nv -n)"
-
-      # after nv -p set above
-      eval 'envy_domain_'$(nv -d)'="$(nv -n)=$(nv -p)"'
+      done <"$ENVY_HOME/$(nv -nn)"
     done
     ;;
 
   pattern | -p)
+    shift && case "$1" in
+    '') nv -pp "$@" ;;
+    -a) shift && nv -pa "$@" ;;
+    -c) shift && nv -pc "$@" ;;
+    -h) echo "usage: name -achu" && exit 0 ;;
+    -u) shift && nv -pu "$@" ;;
+    esac
+    ;;
+  -pp)
     if [ -n "${2+x}" ]; then
       envy_pattern="$2"
     else
@@ -143,7 +191,7 @@ EOF
     while IFS='=' read __ _; do
       [ "$__" ] && unset "$__"
     done <<EOF
-$(nv -g "${2-$(nv -p)}")
+$(nv -g "${2-$(nv -pp)}")
 EOF
     return 0
     ;;
@@ -194,23 +242,28 @@ EOF
 
   save | -s)
     if [ "$2" ]; then
-      ! nv -n "$2" && return 1
+      ! nv -nn "$2" && return 1
     else
-      nv -n
+      nv -nn
     fi
 
-    eval 'envy_domain_'$(nv -d)'="$(nv -n)=$(nv -p)"'
+    eval 'envy_env_'$(nv -dd)'="$(nv -nn)=$(nv -pp)"'
 
-    mkdir -p "$ENVY_HOME/$(nv -d)"
-    printf %s\\n "$(nv -p)" >"$ENVY_HOME/$(nv -n)"
-    nv -g "$(nv -p)" >>"$ENVY_HOME/$(nv -n)"
+    mkdir -p "$ENVY_HOME/$(nv -dd)"
+    printf %s\\n "$(nv -pp)" >"$ENVY_HOME/$(nv -nn)"
+    nv -g "$(nv -pp)" >>"$ENVY_HOME/$(nv -nn)"
     ;;
 
-  unset | -u)
+  unset)
+    shift && case "$1" in
+    *) nv -xu "$@" ;;
+    esac
+    ;;
+  -xu | ux)
     shift
     for __ in "$@"; do
-      if ! [ "$(printf %s "$__" | grep -e "$(nv -p)")" ]; then
-        echo "unset: '$__': could not grep to: '$(nv -p)'" 1>&2
+      if ! [ "$(printf %s "$__" | grep -e "$(nv -pp)")" ]; then
+        echo "unset: '$__': could not grep to: '$(nv -pp)'" 1>&2
       else
         unset "$__"
       fi
