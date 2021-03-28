52a89e940927ab130b1006426e4ef1664453539f
Sean Drucker <srdrucker@gmail.com>
2021-03-27 23:12:38 -0500

    exit

--- shmod.sh
+++ shmod.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=sh
 
 # <repo>@<tag/branch> <file1> <file2>
-# import dockcmd/sh@v0.0.2 docker.sh other.sh ...
+# import github.com/dockcmd/sh@v0.0.2 docker.sh other.sh ...
 #
 # sources other scripts, must not be called in subprocess
 #
@@ -9,13 +9,14 @@
 # lnkn grm shrm.sh
 #
 import() {
-  ! [ "$2" ] && echo "import: $1: Must specify at least one file in repository" 1>&2 &&
+  ! [ "$2" ] &&
+    echo "import: $1: Must specify at least one file in repository" 1>&2 &&
     exit 1
 
   ! _repo=$(clone_opts="--depth 1" shrm clone -- "$1") && exit 1
 
   shift
-  for _file in "$@"; do
+  for _file; do
     # shellcheck source=/dev/null
     ! . "$_repo/$_file" && exit 1
   done
