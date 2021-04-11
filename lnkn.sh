d115fcf1643521c6ca723c9cf5069d0b0d60149f
Sean Drucker <srdrucker@gmail.com>
2021-04-11 13:12:08 -0500

    cleanup

--- lnkn.sh
+++ lnkn.sh
@@ -2,33 +2,42 @@
 
 lnkn_install() {
   # if more than one script, don't link
-  if ! dir=$(grm clone -- "$1") || ! script=$(ls -- "$dir/*.sh") ||
-    [ "$(printf %s "$script" | wc -w)" -ne 1 ]; then
-    ls -- "$dir"
-    exit 1
-  fi
+  dir=$(grm clone -- "$1") || return $?
 
-  base="$(basename -- "$script")"
-  link_name="$lnkn_home/${base%.*}"
+  printf %s\\n "$dir"
+  grm "$1" -perm +111 | while read -r exe; do
+    link="$LNKN_HOME/${exe%.*}"
+    [ -L "$link" ] && rm -- "$link"
+    ln -s "$dir"/"$exe" "$link"
+    lnkn_print "$link"
+  done
 
-  ln -s "$script" "$link_name" &&
-    # reset cache for where executable found in case link covers an existing executable
-    hash -r &&
-    ls -l "$link_name"
+  # reset cache for where executable found in case link covers an existing executable
+  hash -r
 }
 
 lnkn_uninstall() {
-  ! dir="$(grm which -- "$1")" && exit 1
+  dir="$(grm which -- "$1")" || return $?
 
-  grm rm -- "$1" && find "$lnkn_home" -maxdepth 1 | while read -r link; do
+  grm rm -- "$1" && find "$LNKN_HOME" -maxdepth 1 | while read -r link; do
     # any link that references the git directory is removed
     [ "$dir" = "$(dirname -- "$(readlink -- "$link")")" ] &&
-      rm "$link"
+      rm -- "$link"
   done
 }
 
 lnkn_exec() {
-  cd -- "$lnkn_home" && exec "$@"
+  cd -- "$LNKN_HOME" && exec "$@"
+}
+
+lnkn_print() {
+  readlink="$(readlink -- "$1")" &&
+    basename="$(basename -- "$readlink")" &&
+    dirname="$(dirname -- "$readlink")" || return $?
+  case $dirname in
+  "$(grm home)"/*) dirname="${dirname#"$(grm home)"/}" ;;
+  esac
+  printf "%-12s\t%-12s\t%s\\n" "${1#"$LNKN_HOME"/}" "$basename" "$dirname"
 }
 
 lnkn_help() {
@@ -57,78 +66,83 @@ Git Repsoitory and Link Commands
  uninstall   Remove git repository and any associated links
 
 Other Commands
- exec     Execute any linux command in $lnkn_home  
+ exec     Execute any linux command in $LNKN_HOME  
  home     Print home directory for links
  version  Print the version information
  help     Show this help
 EOF
 }
 
-readonly lnkn_home="${LNKN_HOME-$(dirname -- "$0")}"
+readonly LNKN_HOME="${LNKN_HOME-$(dirname -- "$0")}"
 
 case "$1" in
-'') lnkn_exec ls ;;
-
-'-l')
-  find "$lnkn_home" -maxdepth 1 | sort | while read -r item; do
-    [ -L "$item" ] && printf "%-12s\t%s\\n" "${item#"$lnkn_home"/*}" "$(readlink -- "$item")"
+'')
+  find "$LNKN_HOME" -maxdepth 1 | sort | while read -r item; do
+    [ -L "$item" ] && lnkn_print "$item"
   done
   ;;
 
 cmd | c)
   shift && [ "$1" = -- ] && shift
-  ! [ "$2" ] && echo "usage: cmd SOURCE LINK " 1>&2 && exit 1
+  [ "$2" ] || echo "usage: cmd SOURCE LINK " 1>&2 && exit 1
 
-  link=$lnkn_home/$2
+  link="$LNKN_HOME/$2"
   if [ -f "$link" ]; then
-    ls -l "$link"
+    lnkn_print "$link"
     exit 0
   fi
 
   source=$(which -- "$1") &&
     # reset cache for where executable found in case link shadows an existing executable
-    ln -s "$source" "$link" && hash -r && ls -l "$link"
+    ln -s "$source" "$link" && hash -r && lnkn_print "$link"
   ;;
 
-exec) shift && cd -- "$lnkn_home" && exec "$@" ;;
+exec) shift && cd -- "$LNKN_HOME" && exec "$@" ;;
 
 grm | g)
   shift && [ "$1" = -- ] && shift
-  ! [ "$1" ] && echo "usage: lnkn grm SOURCE [LINK] " 1>&2 && exit 1
+  [ "$1" ] || echo "usage: lnkn grm SOURCE [LINK] " 1>&2 && exit 1
 
-  base="$(basename -- "$1")" link_name="$lnkn_home/${2-${base%.*}}"
+  base="$(basename -- "$1")" link_name="$LNKN_HOME/${2-${base%.*}}"
   if [ -f "$link_name" ]; then
-    ls -l "$link_name"
+    lnkn_print "$link_name"
     exit 0
   fi
 
-  if ! count="$(grm find "*/$1" | head -n 2 | wc -l 2>/dev/null)" || [ "$count" -eq 0 ]; then
+  if ! count="$(grm find-all "*/$1" | head -n 2 | wc -l 2>/dev/null)" || [ "$count" -eq 0 ]; then
     echo "grm: $1: File not found" 1>&2
     exit 1
   fi
 
   if [ "$count" -gt 1 ]; then
     echo "grm: multiple sources (limit 10), try 'lnkn grm '*<name>/$1':" 1>&2
-    grm find "$1" | head -n 10 | while read -r file; do
+    grm find-all "$1" | head -n 10 | while read -r file; do
       printf %s\\n "$file"
     done 1>&2
     exit 1
   fi
 
   # link to script
-  ln -s "$(grm home)/$(grm find "*/$1" | head -n 1)" "$link_name" &&
+  ln -s "$(grm home)/$(grm find-all "*/$1" | head -n 1)" "$link_name" &&
     # reset cache for where executable found in case link covers an existing executable
     hash -r &&
     # output link
-    ls -l "$link_name"
+    lnkn_print "$link_name"
   ;;
 
-home) printf %s\\n "$lnkn_home" ;;
+home) printf %s\\n "$LNKN_HOME" ;;
 
 install)
-  shift  && for repo; do
-    lnkn_install "$repo"
-  done
+  shift
+  if [ "$1" = -n ]; then
+    shift && for repo; do
+      grm clone-- "$repo"
+    done
+  else
+    for repo; do
+      lnkn_install "$repo"
+    done
+  fi
   ;;
 
 uninstall)
@@ -139,8 +153,8 @@ uninstall)
 
 which | w)
   shift && [ "$1" = -- ] && shift
-  [ "${1+x}" ] && for param; do
-    [ -L "$lnkn_home/$param" ] && printf %s\\n "$(readlink "$lnkn_home/$param")"
+  [ "${1+_}" ] && for param; do
+    [ -L "$LNKN_HOME/$param" ] && printf %s\\n "$(readlink "$LNKN_HOME/$param")"
   done
   ;;
 
